aaf869b96521951f5f12df502cdccd22d090b198
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-26 04:47:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   private
 
   def require_login
-    redirect_to controller: 'sessions', action: 'new' unless current_user
+    redirect_to controller: 'sessions', action: 'new' unless session.include? :name
   end
 end
